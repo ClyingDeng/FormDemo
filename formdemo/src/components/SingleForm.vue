bf1bb993398bc3e99ae989bb2b3632c909dbfeda
ClyingDeng <1174915260@qq.com>
2020-04-06 21:59:55 +0800

    表格导入

--- formdemo/src/components/SingleForm.vue
+++ formdemo/src/components/SingleForm.vue
@@ -2,7 +2,8 @@
   <div class="singleForm">
     <div class="form">
       <el-table
-        :data="tableData"
+        v-loading="loading"
+        :data="list"
         stripe
         border
         :header-cell-style="{'background-color':'#ccc','color':'#333'}"
@@ -10,7 +11,7 @@
       >
         <el-table-column prop="type" label="类型" min-width="30%"></el-table-column>
         <el-table-column prop="name" label="姓名" min-width="30%"></el-table-column>
-        <el-table-column prop="date" label="日期" min-width="30%"></el-table-column>
+        <el-table-column prop="date" :formatter="dateFormat" c label="日期" min-width="30%"></el-table-column>
         <el-table-column prop="address" label="地址"></el-table-column>
       </el-table>
     </div>
@@ -20,50 +21,44 @@
 <script>
 export default {
   name: "singleForm",
+  props: {
+    list: Array
+  },
+  mounted() {
+    console.log("asasas", this.list);
+  },
   data() {
     return {
-      tableData: [
-        {
-          type: "劳动组1",
-          date: "2016-05-02",
-          name: "王小虎1",
-          address: "上海市普陀区金沙江路 1778 弄"
-        },
-        {
-          type: "劳动组1",
-          date: "2016-05-04",
-          name: "李四",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          type: "劳动组1",
-          date: "2016-05-02",
-          name: "王五",
-          address: "上海市普陀区金沙江路 1348 弄"
-        },
-        {
-          type: "劳动组2",
-          date: "2016-05-01",
-          name: "赵二",
-          address: "上海市普陀区金沙江路 13 弄"
-        },
-        {
-          type: "劳动组2",
-          date: "2016-05-01",
-          name: "王六",
-          address: "上海市普陀区金沙江路 111 弄"
-        },
-        {
-          type: "劳动组1",
-          date: "2016-05-03",
-          name: "张三",
-          address: "上海市普陀区金沙江路 112 弄"
-        }
-      ]
+      loading: false
     };
   },
-  mounted() {},
-  methods: {}
+  watch: {
+    list(val) {
+      console.log("变啊变！", val);
+      if (val.length > 0) {
+        this.loading = false;
+      } else {
+        this.loading = true;
+      }
+    }
+  },
+  methods: {
+    dateFormat(numb, format) {
+      const time = new Date((numb - 1) * 24 * 3600000 + 1);
+      time.setYear(time.getFullYear() - 70);
+      const year = time.getFullYear() + "";
+      const month = time.getMonth() + 1 + "";
+      const date = time.getDate() - 1 + "";
+      if (format && format.length === 1) {
+        return year + format + month + format + date;
+      }
+      return (
+        year +
+        (month < 10 ? "0" + month : month) +
+        (date < 10 ? "0" + date : date)
+      );
+    }
+  }
 };
 </script>
 
@@ -71,7 +66,6 @@ export default {
 .singleForm {
   width: 100%;
   height: 100%;
-  // background-color: #ff0;
   .form {
     width: 80%;
     height: 100%;
